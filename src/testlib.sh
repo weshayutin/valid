0e14d5e39628c966955a2d422cab0f0b6511d5d0
wes hayutin <whayutin@redhat.com>
2011-06-29 08:31:42 -0400

    added memory var to rc.local

--- src/testlib.sh
+++ src/testlib.sh
@@ -803,7 +803,7 @@ function setup_rc.local()
 {
 	echo "####################### cat of /etc/rc.local ##################" >> $LOGFILE
 	echo "cd /root/valid/src" >> /etc/rc.local
-	echo "./image_validation_postreboot.sh --imageID=asdf --RHEL=$RHELV --full-yum-suite=no --skip-questions=yes --bugzilla-username=$BUG_USERNAME --bugzilla-password=$BUG_PASSWORD --bugzilla-num=$BUGZILLA --failures=$FAILURES >> /var/log/messages" >> /etc/rc.local
+	echo "./image_validation_postreboot.sh --imageID=asdf --RHEL=$RHELV --full-yum-suite=no --skip-questions=yes --bugzilla-username=$BUG_USERNAME --bugzilla-password=$BUG_PASSWORD --bugzilla-num=$BUGZILLA --failures=$FAILURES --memory=$MEM_HWP >> /var/log/messages" >> /etc/rc.local
 	cat /etc/rc.local >> $LOGFILE
 
 echo "####################### cat of /etc/rc.local ##################" >> $LOGFILE
